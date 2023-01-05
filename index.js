d86ad47f76fadc431f05ec8e0966b5bd0a9fe068
abir-25 <abirbgc25@gmail.com>
2023-01-06 00:21:31 +0600

    user PUT added

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
     //await client.connect();
     servicesCollection = client.db("doctors_portal").collection("services");
     bookingCollection = client.db("doctors_portal").collection("bookings");
+    userCollection = client.db("doctors_portal").collection("users");
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -35,6 +36,18 @@ async function run() {
       const query = { patient: patient };
       const bookings = await bookingCollection.find(query).toArray();
       res.send(bookings);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
     });
 
     app.post("/booking", async (req, res) => {
